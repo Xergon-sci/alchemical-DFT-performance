d97a9b8d62057770ec0b44047b76ffdd22fddb41
M. Jacobs <jacobs.michiel@icloud.com>
2024-06-26 21:14:17 +0200

    updated to find packages

--- setup.py
+++ setup.py
@@ -11,23 +11,6 @@ setup(
     author_email='michiel.jacobs@vub.be',
     description='The power version of alchemical DFT for installation on HPC infrastructure.',
     url='https://github.com/Xergon-sci/alchemical-DFT-performance',
-    project_urls={
-        'Bug Tracker': 'https://github.com/Xergon-sci/alchemical-DFT-performance/issues',
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={'': 'src'},
-    packages=find_packages(where="src"),
-    include_package_data=True,
-    python_requires=">=3.10",
-    install_requires=[
-        'Click',
-    ],
-    entry_points={
-        'console_scripts': [
-            'adftp = adft_performance.cli:cli',
-        ],
-    },
+    packages=find_packages(),
+    python_requires='>=3.6',
 )
