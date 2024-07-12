0b88c2a03f326141e3628d0dafa34d95d8c0259c
M. Jacobs <jacobs.michiel@icloud.com>
2024-07-12 11:32:44 +0200

    added print statements

--- adftPerformance/models/MJ1.py
+++ adftPerformance/models/MJ1.py
@@ -133,6 +133,7 @@ class MJ1_Preprocessor(Preprocessor):
         obconversion = OBConversion()
         obconversion.SetOutFormat('xyz')
         xyz = obconversion.WriteString(mol, True)
+        print('xyz: ', xyz)
         xyz = '\n'.join(xyz.split('\n')[2:])
         coords = []
         atoms = []
@@ -148,6 +149,7 @@ class MJ1_Preprocessor(Preprocessor):
         coords = np.asarray(coords, dtype=np.float32)
 
         x = CoulombMatrix.generate(atoms=atoms, xyz=coords)
+        print('Coulombmatrix: ', x)
         x = CoulombMatrix.pad_matrix(x, size=33)
         x = CoulombMatrix.normalize(x, negative_dimensions=86, positive_dimensions=14)
         return np.expand_dims(x, axis=3)
